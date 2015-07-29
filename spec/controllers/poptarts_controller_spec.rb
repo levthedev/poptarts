882665c752935da80859f8b5f5677d9ee0ad260c
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-29 09:54:05 -0600

    show test

--- spec/controllers/poptarts_controller_spec.rb
+++ spec/controllers/poptarts_controller_spec.rb
@@ -15,5 +15,18 @@ describe PoptartsController do
       expect(poptart['flavor']).to eq('strawberry')
       expect(poptart['sprinkles']).to eq('red')
     end
+
+    it 'returns a poptart' do
+      poptart = Poptart.create(flavor: 'smores', sprinkles: 'none')
+      get :show, id: poptart.id, format: :json
+
+      expect(response).to have_http_status(:ok)
+      poptart_response = JSON.parse(response.body)
+
+     # poptart = poptart_response.first
+
+     # expect(poptart['flavor']).to eq('smores')
+     # expect(poptart['sprinkles']).to eq('none')
+    end
   end
 end
