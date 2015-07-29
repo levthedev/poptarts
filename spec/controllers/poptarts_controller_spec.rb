4cad2bf420ad9fe56c1cb6410262a85c95ef0299
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-29 09:43:31 -0600

    poptart assertions

--- spec/controllers/poptarts_controller_spec.rb
+++ spec/controllers/poptarts_controller_spec.rb
@@ -3,7 +3,17 @@ require 'rails_helper'
 describe PoptartsController do
   context'#index' do
     it 'returns all the poptarts' do
+      Poptart.create(flavor: 'strawberry', sprinkles: 'red')
+
       get :index, format: :json
+
+      expect(response).to have_http_status(:ok)
+      poptarts = JSON.parse(response.body)
+      expect(poptarts.count).to eq(1)
+
+      poptart = poptarts.first
+      expect(poptart['flavor']).to eq('strawberry')
+      expect(poptart['sprinkles']).to eq('red')
     end
   end
 end
